099aaca56b5cfa833f89d95020561eed4dbbdc28
Zach Taylor <ztayut2tx@gmail.com>
2021-01-18 17:33:02 -0600

    created initial user prompts

--- server.js
+++ server.js
@@ -7,4 +7,57 @@ var connection = mysql.createConnection({
     user: "root",
     password: "root",
     database: "employee_db"
-)};
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    startSearch();
+});
+
+function startSearch() {
+    inquirer.prompt({
+        name: "Select",
+        type: "list",
+        message: "Please Select an Action",
+        choices: [
+            "New Department",
+            "New Employee Role",
+            "Add New Employee",
+            "View Employee",
+            "View Employee Roles",
+            "View Department",
+            "Update Employee Info"
+        ]
+    }).then(function (data) {
+        switch (data.Select) {
+            case "New Department":
+            createDepartment();
+             break;
+
+            case "New Employee Role":
+            createNewRole();
+            break;
+
+            case "Add New Employee":
+            createNewEmp();
+            break;
+
+            case "View Department":
+            viewDepartment();
+            break;
+
+            case "View Employee Role":
+            viewEmpRoles();
+            break;
+
+            case "View Employee":
+            viewEmp();
+            break;
+
+            case "Update Employee Info":
+            updateEmp();
+            break;
+            
+        }
+    });
+}
